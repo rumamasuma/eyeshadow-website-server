7995e406cce8ff6af32a7fcc9b5ecf559993bcca
Ruma Masuma <ruma.masuma@gmail.com>
2021-11-12 19:49:31 +0600

    server

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ try{
     const database = client.db("eyeShadowPalette");
     const productCollection = database.collection("products");
     const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
     const reviewsCollection = database.collection("reviews");
 
     // GET products API
@@ -40,6 +41,36 @@ app.get('/products/:id' , async(req, res)=>{
     res.json(product);
 
 })
+// order get by GET API
+app.get('/orders', async(req, res)=>{
+   const cursor = ordersCollection.find({});
+   const orders = await cursor.toArray();
+   res.json(orders);
+})
+
+// get single order by email
+// app.get('/orders', async(req, res) => {
+//   const email = req.query.email; 
+//   const query = {email: email};
+//   console.log(query);
+//   const cursor = ordersCollection.find(query);
+//   const orderProduct = await cursor.toArray();
+//   res.json(orderProduct);
+// })
+app.get("/orders/:email", async (req, res) => {
+  console.log(req.params.email);
+  const result = await ordersCollection.find({ email: req.params.email }).toArray();
+  res.send(result);
+});
+
+// order send by POST API
+ app.post('/orders', async(req, res) =>{
+   const order = req.body;
+  //  console.log(order);
+  const result = await ordersCollection.insertOne(order);
+  // console.log(result);
+   res.json(result);
+ })
 }
 finally{
     // await client.close();
